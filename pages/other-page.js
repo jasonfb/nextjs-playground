0880030ee6d57f879a85aaabe532e9fb4a44c71e
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-04 19:45:49 -0400

    adds HEAD tag using <Head> component

--- pages/other-page.js
+++ pages/other-page.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Head from 'next/head';
 
 export default function OtherPage () {
   return (
     <>
-    <h1>Other page</h1>
-    <Link href={"/"}>&laquo;back to home</Link>
+      <Head>
+        <title>This is the other page</title>
+      </Head>
+      <h1>Other page</h1>
+      <Link href={"/"}>&laquo;back to home</Link>
     </>
   )
 }
