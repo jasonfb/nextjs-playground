00a30a9341aa0f272cb13a3952706868a4f88082
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-04 19:44:36 -0400

    adds basic image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
-
+import Image from 'next/image';
 export default function Home() {
   return (
     <><h1>Hello Next JS</h1>
 
       <Link href={"other-page"}>Other page</Link>
+
+      <Image
+        src="/images/Morgan-Lee-Avatar.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Morgan Lee"
+      />
     </>
   )
 }
