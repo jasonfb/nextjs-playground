b656e5969b8499855b39c1c82994481bdb63bc0e
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-04 20:17:26 -0400

    adds layout & styles form https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
-import Link from "next/link";
-import Image from 'next/image';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <><h1>Hello Next JS</h1>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello world next js</p>
+        <p>
+          <Link href={"other-page"}>Other page</Link>
 
-      <Link href={"other-page"}>Other page</Link>
-
-      <Image
-        src="/images/Morgan-Lee-Avatar.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Morgan Lee"
-      />
-    </>
-  )
+          <Image
+            src="/images/Morgan-Lee-Avatar.jpg" // Route of the image file
+            height={144} // Desired size with correct aspect ratio
+            width={144} // Desired size with correct aspect ratio
+            alt="Morgan Lee"
+          />
+        </p>
+      </section>
+    </Layout>
+  );
 }
+
