03ef4e5fb52d6ab0f8f6ae9345c529f79bfa901f
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-04 20:10:52 -0400

    basin <Layout> wrapper; basic styles using CSS modules on layout

--- pages/other-page.js
+++ pages/other-page.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import Head from 'next/head';
+import Layout from "../components/layout";
 
 export default function OtherPage () {
   return (
     <>
-      <Head>
-        <title>This is the other page</title>
-      </Head>
-      <h1>Other page</h1>
-      <Link href={"/"}>&laquo;back to home</Link>
+      <Layout>
+        <Head>
+          <title>This is the other page</title>
+        </Head>
+        <h1>Other page</h1>
+        <Link href={"/"}>&laquo;back to home</Link>
+      </Layout>
     </>
   )
 }
